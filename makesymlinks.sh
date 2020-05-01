eb665f39a1a080a01ff575a080db03dccf4ad15d
Paula Ritter <manritter@uos.de>
2020-05-01 17:15:10 +0200

    add .zshrc to files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,7 +6,7 @@
 
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files=".bashrc .vimrc .hyper.js"
+files=".bashrc .vimrc .hyper.js .zshrc"
 
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
@@ -18,7 +18,7 @@ echo "done"
 
 for file in $files; do
   echo "Moving any existing dotfiles from ~ to $olddir"
-  mv ~/$file ~/$olddir/
+  mv ~/$file $olddir/
   echo "Creating symlink to $file in home directory."
   ln -s $dir/$file ~/$file
 done
